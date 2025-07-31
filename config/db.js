400ce3e98c68b96dd7604dedaa2a291b1d7350bf
Amitesh Kumar <120663049+Kumar-Amitesh@users.noreply.github.com>
2025-07-31 13:38:53 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,13 +2,21 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
+// const pool = new Pool({
+//   user: process.env.USER,
+//   host: process.env.HOST,
+//   database: process.env.DATABASE,
+//   password: process.env.PASS,
+//   port: process.env.PORT,
+// });
+
 const pool = new Pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASS,
-  port: process.env.PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
 
 pool.connect()
   .then(() => console.log('Database connected successfully'))
